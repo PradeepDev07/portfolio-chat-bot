17bef1512ecf4e624226f541550592db47c34a8a
PradeepDev07 <pradeep.m.developer@gmail.com>
2026-01-28 13:13:17 +0530

    INIT: init on the git

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <h1>Hello</h1>
+   </>
+  );
+}
+//pcsk_3ZGRZ_LpU643Ua8ZWgf6GTMsiiWK4z1vj6fvytp2zuN2hjRoytx6K7JdxGzBcYExZ1tvz
